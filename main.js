329c447a06e73f8599ad5a1892a1bab84e67f181
Justin Beynon <JMB335@gmail.com>
2020-06-02 21:41:21 -0500

    Added basic operator logic to JS

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function operator(opera, num1, num2){
+	if (opera == "Add"){return num1 + num2}
+		else if (opera === "Subtract"){return num1 - num2}
+		else if (opera === "Multiply"){return num1 * num2}
+		else {return num1 / num2}
+}
