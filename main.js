5c495526f559e79edd9c6e3dc3b37dde9d927846
Justin Beynon <JMB335@gmail.com>
2020-06-14 10:28:45 -0500

    Built most of the calculator functionality. Added buttons

--- main.js
+++ main.js
@@ -5,3 +5,50 @@ function operator(opera, num1, num2){
 		else if (opera === "Multiply"){return num1 * num2}
 		else {return num1 / num2}
 }
+
+let opStaging
+let opType
+let secondNum
+
+let opStagingText = document.querySelector('#opStaging')
+let opTypeText = document.querySelector('#opTypeText')
+let resultsNum = document.querySelector('#results')
+
+
+document.querySelector('#clear').addEventListener('click', function() 
+  {
+  	resultsNum.textContent = "0";
+  	opStaging = null;
+  	opStagingText.textContent = opStaging;
+  	opType = null;
+  	opTypeText.textContent = opType;
+  });
+
+
+document.querySelectorAll('.number').forEach(function(item) {
+item.addEventListener('click', function() 
+  {
+  	if (resultsNum.textContent === "0"){resultsNum.textContent = item.textContent}
+  	else if (opType && opStaging && secondNum != true){
+  		resultsNum.textContent = item.textContent;
+  		secondNum = true;
+  	}
+	else if (parseInt(resultsNum.textContent) > 0 || secondNum === true){resultsNum.textContent += item.textContent}
+  });
+ });
+
+
+document.querySelectorAll('.operator').forEach(function(item) {
+item.addEventListener('click', function() 
+  {
+  	opType = item.id;
+  	opStaging = parseInt(resultsNum.textContent);
+  	opStagingText.textContent = opStaging;
+  	opTypeText.textContent = opType;
+  });
+ });
+ 
+
+
+
+
